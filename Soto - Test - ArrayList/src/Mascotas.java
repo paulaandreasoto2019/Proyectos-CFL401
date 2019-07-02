66421e8f6a5a55033775139ecc29c90531efcca7
Programación <Programacin@DESKTOP-DGA26OH>
2019-07-01 21:28:23 -0300

    Test-ArrayList: Inicio de proyecto Actualizar el programa

---
+++ Soto - Test - ArrayList/src/Mascotas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Programación
+ */
+public class Mascotas {
+    public String nombre;
+    public int edad;
+    public int patas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+    
+    
+}
--- Soto - Test - ArrayList/src/soto/test/arraylist/SotoTestArrayList.java
+++ Soto - Test - ArrayList/src/soto/test/arraylist/SotoTestArrayList.java
@@ -5,6 +5,9 @@
  */
 package soto.test.arraylist;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Programación
@@ -15,7 +18,47 @@ public class SotoTestArrayList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      System.out.println("Buenas tardes");
+      int dato;
+      
+        //System.out.println("Buenas tardes");
+       
+        //ArrayList elem = new ArrayList();
+        ArrayList <Integer> elem2 = new ArrayList();
+        Scanner entrada = new Scanner(System.in);
+        dato = entrada.nextInt();
+        
+        //elem.add("Perro");//.add sirve para agregar a la variable elem
+        //elem.add(4);
+        //elem.add((7));
+       //System.out.println(elem);
+       
+       //System.out.println("Ingrese un número");
+       
+       
+     /*
+        while(dato >= 0){
+         elem2.add(dato);
+         dato = entrada.nextInt(dato);
+        
+         
+         
+           
+           
+       }
+        System.out.println(elem2);   
+         */  
+        try { //esto sirve para guardar los errrores.
+            dato = entrada.nextInt();
+            System.out.println(dato);
+            
+        }  catch (Exception e){
+            System.out.println(e.toString());
+        }           
+        
+        
+       }
+       
+      
     }
     
-}
+
